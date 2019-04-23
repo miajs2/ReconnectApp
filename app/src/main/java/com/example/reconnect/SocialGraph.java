4042ec86ba672e106194cbd4817a5e65a78b985a
Wendy <wshi0810@gmail.com>
2019-04-23 15:43:30 -0500

    Added to graph

--- app/src/main/java/com/example/reconnect/SocialGraph.java
+++ app/src/main/java/com/example/reconnect/SocialGraph.java
@@ -9,9 +9,30 @@ import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class SocialGraph extends View {
+
+    // Hardcoded data to test format
+    String[] names = new String[]{
+            "Alex Baker", "John Jones", "Mary Smith", "Sarah Adams",
+    };
+
+
+    int[] avatars = new int[]{
+            R.drawable.default_avatar, R.drawable.default_avatar, R.drawable.default_avatar, R.drawable.default_avatar,
+    };
+
+    String[] lastConnected = new String[]{
+            "Last Connected: 3 weeks ago", "Last Connected: 5 months ago", "Last Connected: 2 days ago", "Last Connected: 7 weeks ago",
+    };
+
     private final Paint p;
     private final Path path;
+    private final List<Point> points;
+    private final Point selfPoint;
     private final Point point1;
     private final Point point2;
     private final Point point3;
@@ -23,6 +44,22 @@ public class SocialGraph extends View {
         p.setStrokeWidth(5);
         path = new Path();
 
+        List<HashMap<String, String>> aList = new ArrayList<>();
+
+        for (int i = 0; i < 4; i++) {
+            HashMap<String, String> hm = new HashMap<>();
+            hm.put("name", names[i]);
+            hm.put("last_connected", lastConnected[i]);
+            hm.put("avatars", Integer.toString(avatars[i]));
+            aList.add(hm);
+        }
+
+        points = new ArrayList<>();
+
+        for (int i = 0; i < aList.size(); i++) {
+            points.add(new Point(0,0));
+        }
+        selfPoint = new Point();
         point1 = new Point(200, 300);
         point2 = new Point(700, 800);
         point3 = new Point(200, 800);
@@ -52,6 +89,7 @@ public class SocialGraph extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
+
         // draw the edge
         path.reset();
         path.moveTo(point1.x, point1.y);
