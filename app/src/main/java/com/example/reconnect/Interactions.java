bcbbd67ae9c6d9ddc0a26884d1e9efda809f54df
miajs2 <miajs2@illinois.edu>
2019-04-24 21:22:16 -0500

    updated add interaction page

--- app/src/main/java/com/example/reconnect/Interactions.java
+++ app/src/main/java/com/example/reconnect/Interactions.java
@@ -2,38 +2,39 @@ package com.example.reconnect;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Interactions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.interactions);
 
-        Spinner daySpinner = (Spinner) findViewById(R.id.day_spin);
-        // Create an ArrayAdapter using the string array and a default spinner layout
+        Spinner typeSpinner = (Spinner) findViewById(R.id.interaction_type);
         ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
-                R.array.days, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
+                R.array.interactions_array, android.R.layout.simple_spinner_item);
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        daySpinner.setAdapter(adapter1);
-        // Apply the adapter to the spinner
+        typeSpinner.setAdapter(adapter1);
 
-        Spinner monthSpinner = (Spinner) findViewById(R.id.month_spin);
-        // Create an ArrayAdapter using the string array and a default spinner layout
+        Spinner durationSpinner = (Spinner) findViewById(R.id.interaction_duration);
         ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
-                R.array.months, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
+                R.array.duration, android.R.layout.simple_spinner_item);
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        monthSpinner.setAdapter(adapter2);
+        durationSpinner.setAdapter(adapter2);
 
-        Spinner yearSpinner = (Spinner) findViewById(R.id.year_spin);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
-                R.array.years, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        yearSpinner.setAdapter(adapter3);
+        final DatePicker datePicker = (DatePicker) findViewById(R.id.date_picker);
+        final Button button = (Button) findViewById(R.id.createContact);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Toast.makeText(Interactions.this, datePicker.getDayOfMonth()+""+datePicker.getMonth()+""+datePicker.getYear(),Toast.LENGTH_LONG).show();
+            }
+        });
+
 
     }
 }
