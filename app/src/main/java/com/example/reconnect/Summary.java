8d6782be4cf1d46b2f12d7e9de0e1c8e27fd762e
Wendy <wshi0810@gmail.com>
2019-04-26 12:05:05 -0500

    Crop avatar on summary

--- app/src/main/java/com/example/reconnect/Summary.java
+++ app/src/main/java/com/example/reconnect/Summary.java
@@ -4,9 +4,14 @@ import android.content.Intent;
 import androidx.annotation.NonNull;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Spinner;
@@ -78,17 +83,19 @@ public class Summary extends AppCompatActivity {
         dropdownHistory.setAdapter(historyAdapter);
 
         // temporary list of contacts
-        List<HashMap<String, String>> aList = new ArrayList<>();
+        List<HashMap<String, Object>> aList = new ArrayList<>();
 //        dataManager.addInteractionRecord("2019-04-21","30 minutes","Phone","","Philip","Jones");
         myInteractions = dataManager.getAllInteractions();
 
         for (Communication interaction: myInteractions) {
-            HashMap<String, String> hm = new HashMap<>();
+            HashMap<String, Object> hm = new HashMap<>();
             Contact c = dataManager.getNameFromID(interaction.contact_id);
             String name = c.first_name + " " + c.last_name;
+            Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.alex);
+            bm = Helper.getCroppedBitmap(bm);
             hm.put("name", name);
             hm.put("date", interaction.date);
-            hm.put("avatars", Integer.toString(R.drawable.alex));
+            hm.put("avatars", bm);
             hm.put("mode", Integer.toString(Helper.getModeIcon(interaction.type)));
             hm.put("duration", interaction.duration);
             aList.add(hm);
@@ -98,6 +105,21 @@ public class Summary extends AppCompatActivity {
         int[] to = {R.id.name, R.id.date, R.id.avatar, R.id.mode, R.id.duration};
 
         SimpleAdapter simpleAdapter = new SimpleAdapter(getBaseContext(), aList, R.layout.interaction_template_summary, from, to);
+        simpleAdapter.setViewBinder(new SimpleAdapter.ViewBinder(){
+
+            @Override
+            public boolean setViewValue(View view, Object data,
+                                        String textRepresentation) {
+                if( (view instanceof ImageView) & (data instanceof Bitmap) ) {
+                    ImageView iv = (ImageView) view;
+                    Bitmap bm = (Bitmap) data;
+                    iv.setImageBitmap(bm);
+                    return true;
+                }
+                return false;
+
+            }
+        });
         ListView androidListView = (ListView) findViewById(R.id.summary_list);
         androidListView.setAdapter(simpleAdapter);
     }
