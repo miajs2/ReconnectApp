6751d91da7d179c29df2d03dcf7d10274fd871ba
Wendy <wshi0810@gmail.com>
2019-04-25 19:33:20 -0500

    Update button style on add contact

--- app/src/main/java/com/example/reconnect/Interactions.java
+++ app/src/main/java/com/example/reconnect/Interactions.java
@@ -1,10 +1,8 @@
 package com.example.reconnect;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
--- app/src/main/java/com/example/reconnect/TimelineAdapter.java
+++ app/src/main/java/com/example/reconnect/TimelineAdapter.java
@@ -1,15 +1,13 @@
 package com.example.reconnect;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import java.util.List;
 
